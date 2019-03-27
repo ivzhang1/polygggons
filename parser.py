e5adcfc827e386902bb763fc543d82a779406e16
ivzhang1 <izhang1@stuy.edu>
2019-03-27 17:17:33 -0400

    edit so that clear clears polygon matrix

--- parser.py
+++ parser.py
@@ -11,13 +11,13 @@ The file follows the following format:
 
          sphere: add a sphere to the POLYGON matrix -
                  takes 4 arguemnts (cx, cy, cz, r)
-         torus: add a torus to the POLYGON matrix - 
+         torus: add a torus to the POLYGON matrix -
                 takes 5 arguemnts (cx, cy, cz, r1, r2)
-         box: add a rectangular prism to the POLYGON matrix - 
-              takes 6 arguemnts (x, y, z, width, height, depth)	    
+         box: add a rectangular prism to the POLYGON matrix -
+              takes 6 arguemnts (x, y, z, width, height, depth)
          clear: clears the edge and POLYGON matrices
 
-	 circle: add a circle to the edge matrix - 
+	 circle: add a circle to the edge matrix -
 	         takes 4 arguments (cx, cy, cz, r)
 	 hermite: add a hermite curve to the edge matrix -
 	          takes 8 arguments (x0, y0, x1, y1, rx0, ry0, rx1, ry1)
@@ -136,7 +136,8 @@ def parse_file( fname, edges, polygons, transform, screen, color ):
 
         elif line == 'clear':
             edges = []
-            
+            polygons = []
+
         elif line == 'display' or line == 'save':
             clear_screen(screen)
             draw_lines(edges, screen, color)
@@ -145,5 +146,5 @@ def parse_file( fname, edges, polygons, transform, screen, color ):
                 display(screen)
             else:
                 save_extension(screen, args[0])
-            
+
         c+= 1
